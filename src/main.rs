af8c7328dfc2ee0b605f2ab41b72449cef5731d4
Rubens Brandao <git@rubens.io>
2023-08-08 10:36:11 -0300

    Include the DWT

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,8 @@ fn main() {
     let svds = [
         // core peripherals for cortex-m4
         Path::new("/home/rbran/src/arm-generic-svd/Cortex-M4.svd"),
+        // the DWT
+        Path::new("/home/rbran/src/arm-generic-svd/Cortex-M4-DWT.svd"),
         // peripherals for nrf52
         Path::new("/home/rbran/src/nrf-pacs/svds/nrf52832.svd"),
     ];
